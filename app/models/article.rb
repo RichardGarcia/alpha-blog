67202fc9ecc93823556ce688a23ef5d3f12233f1
richardgarcia <richardgarciaph@gmail.com>
2016-04-05 00:57:42 +1200

    add association between users and articles, add debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,18 @@
 class Article < ActiveRecord::Base
-  # to prevent blank or null.
-  #  min and max input
-  validates :title, presence: true, length: {minimum: 3, maximum: 50}
-  validates :description, presence: true, length: {minimum: 10, maximum: 500}
+  # association
+  belongs_to :user
+
+  # validations
+  # to prevent blank or null
+  # min and max input
+  validates :title,
+            presence: true,
+            length: {minimum: 3, maximum: 50}
+
+  validates :description,
+            presence: true,
+            length: {minimum: 10, maximum: 500}
+
+  validates :user_id,
+            presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
+  # change text to lowercase
+  before_save { self.email = email.downcase }
 
+  # association
+  has_many :article
+
+  # regex for email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  # validations
   validates :username,
             presence: true,
             length: { minimum: 3, maximum: 25 },
