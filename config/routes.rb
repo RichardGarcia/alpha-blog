9d1c1ff135a7ad43738de4901cbf6d3e7573a177
richardgarcia <richardgarciaph@gmail.com>
2016-04-19 20:28:03 +1200

    start unit testing

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,16 @@ get 'about', to: 'pages#about'
 
 # will give new articles paths / routes
 resources :articles
+resources :users, except: [:new]
+resources :categories, except: [:destroy]
 
 get 'signup', to: 'users#new'
-resources :users, except: [:new]
 
 get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
